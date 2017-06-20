234b28afb268f9566841cc2651c87e095dba8bde
Mark Brocato <mark.brocato@sencha.com>
2017-06-20 14:40:33 -0400

    Added sparkline

--- src/StocksGrid.js
+++ src/StocksGrid.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import stocks from './data/stocks';
-import { Grid, Column, Button, RendererCell, Container } from '@extjs/ext-react';
+import { Grid, Column, Button, RendererCell, Container, SparkLineLine } from '@extjs/ext-react';
 
 Ext.require('Ext.Toast');
 
@@ -23,12 +23,25 @@ export default class StocksGrid extends Component {
         Ext.toast(`Buy ${record.get('symbol')}`);
     }
 
+    renderTicks = (ticks) => {
+        return (
+            <SparkLineLine 
+                values={ticks} 
+                height={16} 
+                tipTpl='Price: {y:number("0.00")}'
+            />
+        )
+    }
+
     render() {
         return (
             <Grid store={this.store}>
                 <Column renderer={this.actionsRenderer}/>
                 <Column dataIndex="name" text="Name" width={300}/>
                 <Column dataIndex="symbol" text="Symbol" renderer={value => <b>{value}</b>}/>
+                <Column dataIndex="ticks" text="Trend" sortable={false}>
+                    <RendererCell forceWidth renderer={this.renderTicks} bodyStyle={{padding: 0}}/>
+                </Column>
                 <Column dataIndex="sector" text="Sector" width={200}/>
                 <Column dataIndex="industry" text="Industry" width={350}/>
             </Grid>
